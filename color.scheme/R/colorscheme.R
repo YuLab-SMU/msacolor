aca0f4c7789eaeeb25e5541abd0b27c2576eab3a
huanghuinana <1185796994@qq.com>
2019-05-16 09:56:01 +0800

    optimize code(the fourth time)

--- color.scheme/R/colorscheme.R
+++ color.scheme/R/colorscheme.R
@@ -24,26 +24,27 @@ color_scheme<-function(seq,scheme_df){
     y <- rep("white", length(x))
     names(y) <- names(x)
     r <- x/sum(x)
-    for (pos in seq_along(x)) {
+    y<-sapply(seq_along(x),function(pos){
       char <- names(x)[pos]
       i <- grep(char, col_df$re_position)
       for (j in i) {
         rr<-r[re_gp[[j]]]
         rr<-rr[!(is.na(rr))]     ##get frequency of the character in the col_df$re_gp[j]
-
+        
         ##the situation without , in 're_gp'
         if (sum(rr) > col_df$thred[j] && col_df$type[j]=="combined" && length(rr)>1) {
           y[pos] <- col_df$colour[j]
           break
         }
-
+        
         ##the situation with , in 're_gp'
         if(any(rr>col_df$thred[j])  && col_df$type[j]=="individual"){
           y[pos] <- col_df$colour[j]
           break
         }
       }
-    }
+      return(y[pos])
+    })
     return(y)
   })
 
